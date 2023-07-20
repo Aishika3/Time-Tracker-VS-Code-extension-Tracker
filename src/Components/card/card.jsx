f75b6d12dfd8a79f40a556bfcb5dcee4ef939f69
CS-parth <parth.vijy@gmail.com>
2023-07-20 16:38:32 +0530

    Added Card Component

--- src/Components/card/card.jsx
+++ src/Components/card/card.jsx
@@ -1,49 +1,30 @@
 import './card.css'
-import React, { useState } from 'react'
+import React from 'react'
 
-export default function Card(props){
-    // console.log(props)
-
-        const [isFlipped,setisFlipped] = useState(false);
-        const handleFlip = ()=>{
-            setisFlipped(!isFlipped);
-        }
-
+export default function Card({ logo, title, author, img, description, btn }){
+    
         return (
-            <>
+            
             <div className="card-container">
-                    <div className={`card ${isFlipped == true ? "rotate" : ""}`}>
-                        <div className="frontside">
-                                    <section className="card-row">
-                                        <img className="logo" src={props.logo} alt="logo-img"/>
-                                        <div className="title"><b>{props.title}</b></div>
-                                    </section>
-                                    <div className="auther">{props.auther}</div>
-                                    <section className="card-row">
-                                        <img src={props.img} alt="" className="card-img" />
-                                        <button type="button" onClick={handleFlip} className="flip-btn">FLIP</button>
-                                    </section>
-                                    <div className="description">
-                                        <p>{props.description}</p>
-                                    </div>
-                                    <footer>
-                                            <button className="btn-functional" type='button'>Check out {props.btn}</button>
-                                    </footer>
-                        </div>
-                        <div className="backside">
-                                <div className="card-row">
-                                <button type="button" onClick={handleFlip} className="back-btn">
-                                    &larr; &nbsp; Back
-                                </button>
-                                </div>
-                                <div className="back-content">
-                                    {props.backContent}
-                                </div>
-                        </div>
-                    </div>
-
+              <div className="card">
+                  <section className="card-row">
+                      <img className="logo" src={logo} alt="logo-img"/>
+                      <div className="title"><b>{title}</b></div>
+                  </section>
+                  <div className="auther">{author}</div>
+                  <section className="card-row">
+                      <img src={img} alt="" className="card-img" />
+                  </section>
+                  <div className="description">
+                      <p>{description}</p>
+                  </div>
+                  <footer>
+                      <button className="btn-functional">
+                          {btn}
+                      </button>
+                  </footer>
+              </div>
             </div>
-            </>
         )
 
 }
