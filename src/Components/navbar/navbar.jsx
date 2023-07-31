b8ce648812f5e09771c6fcbf635b03dbe90444bc
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-07-31 11:06:35 -0700

    added more styling to navbar; conditional className and styling based on 'active' state of navlinks

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-// import Home from "../../Projects/Home/home";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 
 const navbar = () => {
   return (
     <div className="navbar-wrapper">
-      <NavLink to="/" className="navlink">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "active" : "navlink")}
+      >
         Home
       </NavLink>
-      <NavLink to="/list-of-projects" className="navlink">
+      <NavLink
+        to="/list-of-projects"
+        className={({ isActive }) => (isActive ? "active" : "navlink")}
+      >
         List of Projects
       </NavLink>
-      <NavLink to="/visual-representation" className="navlink">
+      <NavLink
+        to="/visual-representation"
+        className={({ isActive }) => (isActive ? "active" : "navlink")}
+      >
         Visual Respresentation
       </NavLink>
-      <NavLink to="/log-in" className="navlink">
+      <NavLink
+        to="/log-in"
+        className={({ isActive }) => (isActive ? "active" : "navlink")}
+      >
         Log In
       </NavLink>
     </div>
