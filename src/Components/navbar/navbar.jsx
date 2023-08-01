0dd68328bb93946a76799a8f035bbc71d1aba88a
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-08-01 16:44:08 -0700

    installed react-icons; added media queries for reponsiveness; hamburger menu displays modal onClick; hamburger menu displays at min-width: 375px and hidden at min-width: 768px

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 import "./navbar.css";
 
-const navbar = () => {
+export const NavbarLinks = ({ className, modalHandler }) => {
   return (
-    <div className="navbar-wrapper">
+    <div className={className}>
       <NavLink
         to="/"
         className={({ isActive }) => (isActive ? "active" : "navlink")}
+        onClick={modalHandler}
       >
         Home
       </NavLink>
       <NavLink
         to="/list-of-projects"
         className={({ isActive }) => (isActive ? "active" : "navlink")}
+        onClick={modalHandler}
       >
         List of Projects
       </NavLink>
       <NavLink
         to="/visual-representation"
         className={({ isActive }) => (isActive ? "active" : "navlink")}
+        onClick={modalHandler}
       >
         Visual Respresentation
       </NavLink>
       <NavLink
         to="/log-in"
         className={({ isActive }) => (isActive ? "active" : "navlink")}
+        onClick={modalHandler}
       >
         Log In
       </NavLink>
@@ -33,4 +38,30 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export const MobileNav = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const modalHandler = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div className="mobile-nav-wrapper">
+      <GiHamburgerMenu onClick={modalHandler} />
+      {isModalOpen && (
+        <NavbarLinks className="mobile-wrapper" modalHandler={modalHandler} />
+      )}
+    </div>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <div>
+      <MobileNav />
+      <NavbarLinks className="navbar-wrapper" />
+    </div>
+  );
+};
+
+export default Navbar;
